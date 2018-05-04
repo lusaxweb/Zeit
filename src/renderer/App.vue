343e66287ee93213b67c3047c2c8b5e2f947139f
ldrovira <31676496+luisDanielRoviraContreras@users.noreply.github.com>
2018-05-04 08:55:25 -0400

    change and commit prueba

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -9,7 +9,7 @@
   export default {
     name: 'zeit',
     mounted () {
-      console.log('Maluma Baby!')
+      console.log('Maluma Baby 2!')
       firebase.database().ref('/hello').on('value', function (snapshot) {
         console.log(snapshot.val())
       })
