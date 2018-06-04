79ea66f6a63c791038f542f8e732730011080002
Jarvis <jarvijavier@gmail.com>
2018-06-04 13:04:30 -0400

    add flexbox

--- src/renderer/pages/login/Login.vue
+++ src/renderer/pages/login/Login.vue
@@ -1,27 +1,32 @@
 <template>
   <div class="con-login" id="wrapper">
     <follow/>
-    <div class="con-img">
-      <img src="/static/img/logo-zeit.png" alt="">
-    </div>
-    <div class="con-register-login">
-      <Login @register-click="register=true"  v-if="!register"/>
-      <Register @volver-click="register=false"  v-else/>
-    </div>
-    <div class="con-footer" id="footer">
-      <footer>
-        <div  class="con-copyright"> <p>Copyright &copy; 2018 Lusax Web</p> </div> 
-        <div class="con-version" >  <p >V.0.0.1 </p> </div> 
-      </footer>
-    </div>
+    <div class="con-registroLogin">   
+      <div class="con-img">
+        <img src="/static/img/logo-zeit.png" alt="">
+      </div>
 
+      <div class="con-register-login">
+        <Login @register-click="register=true"  v-if="!register"/>
+        <Register @volver-click="register=false"  v-else/>
+      </div>
     </div>
+    
+    <div class="con-footer" id="footer">
+      <footer >
+        <div class="con-copyright"> <a href="http://www.lusaxweb.com.ve/" target="_blank">Copyright &copy; 2018 Lusax Web</a> </div>
+        <div class="con-version" >  <p >V {{version}} </p> </div> 
+      </footer>      
+    </div>
+  </div>
+
 </template>
 
 <script>
   import Register from './_register.vue'
   import Login from './_login.vue'
   import follow from './follow.vue'
+  import version from '../../../../package.json'
   export default {
     name: 'landing-page',
     components: {
@@ -31,7 +36,8 @@
     },
     data () {
       return {
-        register: false
+        register: false,
+        version: version.version
       }
     },
     methods: {
@@ -67,40 +73,59 @@
     width: 150px;
   }
   .con-login {
-    display: flex;
-    flex-flow: column nowrap;
+    display: flex;    
     align-items: center;
     justify-content: center;
     height: 100vh;
+    position: relative;
+    width: 100%;
   }
+
   .con-register-login {
     display: flex;
     align-items: center;
     justify-content: center;
-    /*padding-left: 50px;*/
     border-left: 1px solid rgba(0, 0, 0, 0.050)
   }
 
   .con-footer {
-    /*clear:both*/
+    bottom:0px;
+    width:100%;
+    padding: 20px;
     position: absolute;
-    bottom:20px;
-    width:95%;
-    
   }
 
+  .con-footer footer {
+    display: flex;
+    justify-content: space-between;
+    width:100%;
+}
   .con-copyright {
-    float: left;
     font-family: verdana;
     font-size: 10px;
+
   }
   .con-version {
-    left:  50%;
-    float: right;
     font-family: verdana;
     font-size: 10px;
   }
 
- 
+ .con-footer:before {
+     width: 8px;     
+     background:white;
+     content: '';
+     position: absolute;
+     left: 0px;
+     top: 29%;
+     height: 45%;
+     border-radius: 0px 5px 5px 0px;
+}
+
+  .con-copyright a {
+    color:white;
+}
   
+  .con-registroLogin{
+    width: calc(100% - 200px);
+  }
 </style>
--- src/renderer/pages/login/follow.vue
+++ src/renderer/pages/login/follow.vue
@@ -1,9 +1,9 @@
 <template lang="html">
-  <div>
-    <a href="https://github.com/lusaxweb/Zeit">Github</a>
-    <a href="https://twitter.com/LusaxWeb" >Twitter</a>
-    <a href="https://www.behance.net/ManuelRovira" >Behance</a>
-    <a href="https://dribbble.com/ManuelRovira" >Dribbble</a>
+  <div class="con-link">
+    <div class="con-github"><a href="https://github.com/lusaxweb/Zeit" target="_blank">Github</a></div> 
+    <div class="con-twitter"><a href="https://twitter.com/LusaxWeb" target="_blank">Twitter</a></div> 
+    <div class="con-behance"><a href="https://www.behance.net/ManuelRovira" target="_blank">Behance</a></div> 
+    <div class="con-dribbble"><a href="https://dribbble.com/ManuelRovira" target="_blank">Dribbble</a></div> 
   </div>
 </template>
 
@@ -14,5 +14,30 @@ export default {
 </script>
 
 <style lang="css">
+
+.con-link{
+	display: flex;
+	position: relative;
+	height:100%;
+	width:200px ;
+  background-color: #b800e6;  
+  text-align: center;
+	right: 0px;
+  flex-direction:column; 
+}
+
+.con-link a {
+	color:white;
+
+}
+
+.con-github{
+	color:white;
+}
+
+
+
+
+
 </style>
 
